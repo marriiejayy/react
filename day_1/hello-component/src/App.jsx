70d2a7511fcfbd1b75e55d39ba06dfdec73a23d1
Mariam <madesina011@gmail.com>
2025-10-15 15:59:20 +0100

    m

---
+++ day_1/hello-component/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './Hello';
+
+import './App.css'
+
+function App() {
+  return (
+    <div >
+      <Header />
+    </div>
+  );
+}
+
+export default App
---
+++ day_1/hello-component/src/Hello.jsx
@@ -0,0 +1,11 @@
+function Hello() {
+  return (
+    <header>
+      <h1>Adesina Mariam</h1>
+      <p>Ogun, Nigeria</p>
+      <p>Welcome to my react journey!</p>
+    </header>
+  );
+}
+
+export default Hello;
