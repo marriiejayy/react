01ab0dcea6b414793fc18d5f749f8549afb77d81
Mariam <madesina011@gmail.com>
2025-10-17 16:18:28 +0100

    latest

---
+++ code_along/code_two/src/ProfileCard1.jsx
@@ -0,0 +1,8 @@
+function ProfileCard1() {
+    return (
+        <div>
+            <h2>Chidi Okarfor</h2>
+            <p>Frontend developer</p>
+        </div>
+    );
+}
---
+++ code_along/code_two/src/ProfileCard2.jsx
@@ -0,0 +1,11 @@
+// function ProfileCard(props) {
+//     return (
+//         <div>
+//             <h2>{props.name}</h2>
+//             <p></p>
+//         </div>
+//     );
+// }
+
+// <ProfileCard name="Chidi Okafor" role="Frontend Developer" />
+// <ProfileCard name="Amaka Johnson" role="Backend Developer" />
