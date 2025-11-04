010c67abbc4cfe23df8443f37527ff4903fcf402
Mariam <madesina011@gmail.com>
2025-11-04 16:19:05 +0100

    m

---
+++ code_eleven/react-router/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  
+
+  return (
+   <Routes>
+
+
+   </Routes>
+  )
+}
+
+export default App
